5cd7f0571b63d0f39cf539552ef6bf17c6e14d63
jalloyd804 <jalloyd@deloitte.com>
2023-06-28 09:39:50 -0400

    added logo and styled layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className="absolute inset-0 flex justify-center items-center">
-      Website Content
+    <main className="absolute inset-0 flex justify-center items-center bg-black">
+      <Image
+        className="flex justify-center items-center mx-auto"
+        src="/logo.jpeg"
+        width={250}
+        height={250}
+        alt="Deloitte logo"
+      />
     </main>
   );
 }
